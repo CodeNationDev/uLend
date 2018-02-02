5bb0e8b7d442e6f218eac68971add54569875b63
Manu <manu.stier@me.com>
2018-02-02 14:45:30 +0100

    Added create with documentSnapshot add func toProfile

--- uLendApp/Entities/Item.swift
+++ uLendApp/Entities/Item.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Firebase
 
 
 
@@ -16,12 +17,48 @@ final class Item {
     var uidOwner: String?
     var name: String?
     var description: String?
-    
-    
     var images: [String]?
     var geoposition: GeoPosition?
     
+    init(){}
     
+    
+     
+    init(document: DocumentSnapshot!){
+        
+        
+        //demasiado forzado mejor con guard??
+
+        self.uid = document.data()?["uid"] as? String
+        self.uidOwner = document.data()?["uidOwner"] as? String
+        self.name = document.data()?["name"] as? String
+        self.description = document.data()?["description"] as? String ?? ""
+        
+        //geo: definir con un guard?
+//         esta parte va a hacer crash??
+        let geo = document.data()?["_geoloc"] as! Dictionary<String, Double>
+        self.geoposition = GeoPosition(geo["lng"]!, geo["lat"]!)
+        
+        
+//        images
+        self.images = document.data()?["imagesArray"] as? [String] ?? [String]()
+
+    }
+    
+    
+    func toProfile() -> Profile {
+        var profile = Profile()
+        profile["uid"] = self.uid as AnyObject
+        profile["owner"] = self.uidOwner as AnyObject
+        profile["name"] = self.name as AnyObject
+        profile["description"] = self.description as AnyObject
+        profile["images"] = self.images as AnyObject
+        profile["geoposition"] = self.geoposition?.dataGeoposition() as AnyObject
+        
+        
+        return profile
+        
+    }
     
     
     
