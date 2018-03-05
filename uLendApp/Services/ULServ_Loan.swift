6d9400dbb57b79dcd2ba4ac27b532f36ce1f9b50
Manu <manu.stier@me.com>
2018-03-05 12:31:36 +0100

    change multiple func to one switch

--- uLendApp/Services/ULServ_Loan.swift
+++ uLendApp/Services/ULServ_Loan.swift
@@ -54,57 +54,86 @@ struct ULServ_Loan {
 //        case cancelled    --> modifica el receiver
 //        case denied       -> modifica el owner
 //    }
-
-    private func verifyChangeToAccepted(_ loan: Loan!, _ userID: String!) -> Bool{
+//
+//    private func verifyChangeToAccepted(_ loan: Loan!, _ userID: String!) -> Bool{
+//
+//        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private func verifyChangeToCancel(_ loan: Loan!, _ userID: String!) -> Bool {
+//
+//        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private func verifyChangeToLoaned(_ loan: Loan!, _ userID: String!) -> Bool {
+//
+//        if loan.loanUser?.uid == userID && loan.status == loanstatus.accepted {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private func verifyChangeToReturn(_ loan: Loan!, _ userID: String!) -> Bool {
+//
+//        if loan.loanUser?.uid == userID && loan.status == loanstatus.loaned {
+//            return true
+//        }
+//        return false
+//    }
+//    private func verifyChangeToCancelled(_ loan: Loan!, _ userID: String!) -> Bool {
+//
+//        if loan.loanUser?.uid == userID && loan.status == loanstatus.ordered {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private func verifyChangeToDenied(_ loan: Loan!, _ userID: String) -> Bool {
+//        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
+//            return true
+//        }
+//
+//        return false
+//    }
+    
+    private func verifyChange(_ loan: Loan!, _ userID: String!, _ newStatus: loanstatus!) -> Bool {
+        switch newStatus! {
+        case .cancelled:
+            if loan.loanUser?.uid == userID && loan.status == loanstatus.ordered { return true }
+            break
+            
+        case .accepted:
+            if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered { return true }
+            break
         
-        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
-            return true
+        case .loaned:
+            if loan.ownerUser?.uid == userID && loan.status == loanstatus.accepted { return true }
+            break
+            
+        case .toreturn:
+            if loan.loanUser?.uid == userID && loan.status == loanstatus.loaned { return true }
+            break
+            
+        case .closed:
+            if loan.ownerUser?.uid == userID && loan.status == loanstatus.toreturn { return true }
+            break
+       
+        case .ordered:
+            break
+            
+        case .denied:
+            if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered { return true }
+            break
         }
+        
         return false
     }
-    
-    private func verifyChangeToCancel(_ loan: Loan!, _ userID: String!) -> Bool {
-        
-        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
-            return true
-        }
-        return false
-    }
-    
-    private func verifyChangeToLoaned(_ loan: Loan!, _ userID: String!) -> Bool {
-        
-        if loan.loanUser?.uid == userID && loan.status == loanstatus.accepted {
-            return true
-        }
-        return false
-    }
-    
-    private func verifyChangeToReturn(_ loan: Loan!, _ userID: String!) -> Bool {
-        
-        if loan.loanUser?.uid == userID && loan.status == loanstatus.loaned {
-            return true
-        }
-        return false
-    }
-    private func verifyChangeToCancelled(_ loan: Loan!, _ userID: String!) -> Bool {
-        
-        if loan.loanUser?.uid == userID && loan.status == loanstatus.ordered {
-            return true
-        }
-        return false
-    }
-    
-    private func verifyChangeToDenied(_ loan: Loan!, _ userID: String) -> Bool {
-        if loan.ownerUser?.uid == userID && loan.status == loanstatus.ordered {
-            return true
-        }
-        
-        return false
-    }
-    
-    
-    
-    //reeescribir con un switch para hacerlo más ameno    ^^^^^^^^^^^^
     
     
     func changeStatus(_ loan: Loan!,_ status: loanstatus, completionHandler:@escaping CompletionBool){
