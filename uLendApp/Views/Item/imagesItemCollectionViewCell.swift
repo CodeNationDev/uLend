2df92f21fb6d64f8d08fc961d4b4e5fcf10f1138
Manu <manu.stier@me.com>
2018-03-08 17:21:30 +0100

    cleaning code

---
+++ uLendApp/Views/Item/imagesItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  imagesItemCollectionViewCell.swift
+//  uLendApp
+//
+//  Created by Manu on 8/3/18.
+//  Copyright © 2018 Manu. All rights reserved.
+//
+
+import UIKit
+
+class imagesItemCollectionViewCell: UICollectionViewCell {
+    
+}
